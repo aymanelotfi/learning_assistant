f95da47aba049e206fd9853190dd4ba88093cff0
Lotfi Aymane <aymane.lotfi@student-cs.fr>
2022-04-11 19:05:52 +0200

    markdown

--- static/js/relearn.js
+++ static/js/relearn.js
@@ -1,7 +1,13 @@
 function relearnInit() {
-
-  var textArea = document.getElementById("myTextarea");
-  var editor = CodeMirror.fromTextArea(textArea, {
-    lineNumbers: true
+  // var textArea = document.getElementById("myTextarea");
+  // var editor = CodeMirror.fromTextArea(textArea, {
+  //   lineNumbers: true
+  //   //mode: "htmlmixed"
+  // });
+  const easyMDE = new EasyMDE({element: document.getElementById('myTextarea')});
+  easyMDE.value();
+  easyMDE.codemirror.on("change", () => {
+      console.log(easyMDE.value());
   });
+
 }
